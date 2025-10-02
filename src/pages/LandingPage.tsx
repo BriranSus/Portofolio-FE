ca32ae4edca7d9df9701e5e1d5ce0962ab55a898
BriranSus <alexander.susanto001@binus.ac.id>
2025-10-02 12:29:00 +0700

    Finished Landing Page HTML Structure

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,97 @@
-function LandingPage() {
-    return (
-        <>
-            
-        </>
-    )  
+import React from 'react';
+import { pribadi } from '../utils/data'
+
+class LandingPage extends React.Component {
+    constructor(props: any){
+        super(props)
+
+        this.state = {
+            pribadi: pribadi,
+        }
+    }
+
+    render() {
+        return (
+            <>
+                <header>
+                    {/* <Navbar></Navbar> */}    
+                </header>
+
+                <main>
+                    <div>
+                        {/* Isinya scroll wheel */}
+                    </div>
+                    <section id='description'>
+                        <div>
+                            <h1>My Name is</h1>
+                            <h1>{pribadi.name}</h1>
+                            <div>
+                                {pribadi.description}
+                            </div>    
+                        </div>
+                        <div>
+                            <img src={pribadi.image_description} alt="" />
+                        </div>
+                    </section>
+
+                    <section id='skills'>
+                        <div>
+                            <img src={pribadi.image_skills} alt="" />
+                        </div>
+                        <div>
+                            {/* Isinya graph skill */}
+                        </div>
+                    </section>
+
+                    <section id='projects'>
+                        <div>
+                            {/* Carousel */}
+                        </div>
+                        <div>
+                            {/* Buttons untuk pindah state carousel */}
+                        </div>
+                    </section>
+
+                    <section id='learning'>
+                        <div>
+                            <h1>Currently Learning</h1>
+                            <img src={pribadi.image_learning} alt="" />
+                        </div>
+                    </section>
+                </main>
+
+                <footer>
+                    <div>
+                        <section>
+                            <div>
+                                <h1>Contact me:</h1>
+                                <p>{pribadi.email}</p>
+                                <p>{pribadi.telp}</p>
+                            </div>
+                            <div>
+                                {/* <SocialButton></SocialButton> */}
+                            </div>
+                            <div>
+                                <h1>Navigation</h1>
+                                <a href="#description">About Me</a>
+                                <a href="#skills">My Skills</a>
+                                <a href="#projects">My Projects</a>
+                                <a href="#learning">Future Plans</a>
+                            </div>
+                        </section>
+                        <section>
+                            <div>
+                                <h1>@2025 Alexander Brian Susanto</h1>
+                            </div>
+                            <div>
+                                <h1>Created with Love</h1>
+                            </div>
+                        </section>
+                    </div>
+                </footer>
+            </>    
+        )
+    }
 }
 
 export default LandingPage;
---
+++ src/utils/data.ts
@@ -0,0 +1,15 @@
+import Description from '../assets/Description.svg' 
+import Skills from '../assets/Skills.svg'
+import Learning from '../assets/Golang.svg'
+
+let pribadi = {
+    name: "Alexander Brian Susanto",
+    description: "I’m a motivated aspiring Frontend Developer with expertise in React.js, TypeScript, and TailwindCSS. Experienced in designing, developing, maintaining web-based applications, and managing projects. Seeking to leverage my technical skills to create impactful digital experiences.",
+    image_description: Description,
+    image_skills: Skills,
+    image_learning: Learning,
+    email: "briansusanto144@gmail.com",
+    telp: "+6287877641342",
+}
+
+export { pribadi };
