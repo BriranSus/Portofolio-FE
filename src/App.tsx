7df28eddf097237a415e41e4606b151f7595b587
BriranSus <alexander.susanto001@binus.ac.id>
2025-10-02 11:59:06 +0700

    Added Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
